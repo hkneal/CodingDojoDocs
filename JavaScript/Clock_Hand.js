b22890388c878048b8b9df49a6529375f77ce129
hkneal <hiramk@comcast.net>
2017-04-08 01:33:26 -0700

    Updated ClockHand.js

--- JavaScript/Clock_Hand.js
+++ JavaScript/Clock_Hand.js
@@ -1,11 +1,11 @@
 function clockHandAngles(numSec){
-  var hourAngle = minAngle = secAngle = weekAngle =0;
+  var hourAngle = minAngle = secAngle = weekAngle = weekDegree = 0;
   var numWeeks = numDays = numHours = numMins = 0;
+  numDays = numSec / 86400;
   // determine number of hours and mins
   numHours = numSec / 3600;
   // determine the number of days & reduce hours to 24hours or less
   while(numHours >= 24){
-    numDays++;
     numHours = numHours - 24;
   }
   numMins = (numHours%1)*60;
@@ -16,6 +16,13 @@ function clockHandAngles(numSec){
   secAngle = numSec * 6;
   console.log("Hour hand: " + hourAngle + " degs." + " Minute hand: " +
    minAngle + " degs." + " Second hand: " + secAngle + " degs.");
+
+   while(numDays >= 7){
+     numDays = numDays - 7;
+   }
+  weekDayDegree = 360 / 7;
+  weekAngle = numDays * weekDayDegree;
+  console.log("Week hand: " + weekAngle + " degs.");
 }
 
 clockHandAngles(119730);
